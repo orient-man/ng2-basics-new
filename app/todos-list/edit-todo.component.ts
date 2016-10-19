48058874118fd246adc87ab8c7c83008a581564e
Marcin Malinowski <orientman@gmail.com>
2016-10-19 10:25:55 +0200

    Editing of todo items

--- app/todos-list/edit-todo.component.ts
+++ app/todos-list/edit-todo.component.ts
@@ -1,10 +1,29 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
+
+import { Todo } from './todo'
 
 @Component({
     selector: 'edit-todo',
     templateUrl: 'app/todos-list/edit-todo.component.html'
 })
 export class EditTodoComponent implements OnInit {
+    @Input() todo: Todo;
+    item: Todo;
+    @Output() updated: EventEmitter<Todo> = new EventEmitter<Todo>();
+    @Output() cancelled: EventEmitter<number> = new EventEmitter<number>();
+
     constructor() { }
-    ngOnInit() { }
+    ngOnInit() {
+        this.item = Object.assign({}, this.todo);
+    }
+
+    updateClick() {
+        console.log('Update click...');
+        this.updated.emit(this.item);
+    }
+
+    cancelClick() {
+        console.log('Cancel click...');
+        this.cancelled.emit(this.item.id);
+    }
 }
--- app/todos-list/todos-list.component.ts
+++ app/todos-list/todos-list.component.ts
@@ -8,6 +8,7 @@ import { ALL_TODOS } from './todos.service'
 })
 export class TodosListComponent implements OnInit {
     todos = ALL_TODOS;
+    underEdit: boolean[] = [];
     title: string;
     desc: string;
 
@@ -16,6 +17,10 @@ export class TodosListComponent implements OnInit {
     ngOnInit() {
         console.log('Init... ')
         console.log(this.todos);
+
+        this.title = "";
+        this.desc = "";
+        this.todos.forEach(todo => this.hideEdit(todo.id));
     }
 
     add() {
@@ -26,6 +31,24 @@ export class TodosListComponent implements OnInit {
             description: this.desc,
             isFinished: false
         });
+        this.hideEdit(maxId + 1);
+    }
+
+    edit(id: number) {
+        console.log('Editing...');
+        this.underEdit[id] = true;
+    }
+
+    update(todo: Todo) {
+        console.log('Updating...');
+        this.hideEdit(todo.id);
+        let el = this.todos.find(el => el.id == todo.id);
+        Object.assign(el, todo);
+    }
+
+    hideEdit(id: number) {
+        console.log('Hiding...');
+        this.underEdit[id] = false;
     }
 
     remove(id: number) {
